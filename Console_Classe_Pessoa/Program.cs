5c05c81fadfe147e9391a5eda1dda1bcc8e3319b
Douglas Henrique <douglashas05@gmail.com>
2021-10-14 19:28:23 -0300

    Adicionar arquivos de projeto.

---
+++ Console_Classe_Pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Classe_Pessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
